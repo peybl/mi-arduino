b4ba069206cd95abc273206c9f689ea37883a458
Matthias Reisacher <reisacher.matthias@gmail.com>
2017-05-03 15:21:09 +0200

    Project init.

---
+++ Assets/Scripts/Arduino/MMI/MMI.ino
@@ -0,0 +1,83 @@
+//ultrasonic code from: https://www.tautvidas.com/blog/2012/08/distance-sensing-with-ultrasonic-sensor-and-arduino/
+const int photoresistor_pin = 0; //photoresistor
+const int trigPin = 2; //ultrasonic trigger
+const int echoPin = 4; //ultrasonic echo
+const int buzzer_pin = 7; //passive buzzer
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(photoresistor_pin, INPUT); //photoresistor
+  pinMode(buzzer_pin,OUTPUT); //initialize the buzzer pin as an output
+
+}
+
+void loop()
+{
+   //receiving data from unity
+   if (Serial.available() > 0) {
+         // read the incoming byte:
+         char receivedChar = Serial.read();
+       if('c' == receivedChar)
+       {
+          buzz_crash();
+       }
+     }
+  //photoresistor
+  long photoresistor = analogRead(photoresistor_pin); //read from photoresistor
+  Serial.println(photoresistor);
+  //ultrasonic sensor
+  // establish variables for duration of the ping, 
+  // and the distance result in inches and centimeters:
+  /*
+  long duration, cm;
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(photoresistor);
+  Serial.print(" ");
+  Serial.print(cm);
+  Serial.println();
+  */
+  //delay
+  delay(200);
+}
+
+void buzz_crash()
+{
+  unsigned char i;
+  
+  for(i=0;i<80;i++)
+  {
+  digitalWrite(buzzer_pin,HIGH);
+  delay(1);//wait for 1ms
+  digitalWrite(buzzer_pin,LOW);
+  delay(1);//wait for 1ms
+  }
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+
---
+++ Assets/Scripts/ArduinoConnector.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.IO.Ports;
+
+public class ArduinoConnector : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("COM3", 9600); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+
+    //properties from arduino
+    private int light = 0;
+    public int Light { get { return light; } }
+    private int distance = 0;
+    public int Distance { get { return distance; } }
+
+    private int life = 0;
+    public int Life { set { life = value; } }
+
+    private int soundlength = 300;
+    private int sounddelay = 0;
+
+
+    void Start()
+    {
+        this.OpenStream();
+        stream.ReadTimeout = 400;
+    }
+
+    void Update()
+    {
+        //photoresistor
+        string value = stream.ReadLine(); //Read the information
+        string[] values = value.Split(' ');
+
+        if (Int32.TryParse(values[0], out light))
+        {
+            Debug.Log("light: " + light);
+            
+        }
+
+        if (Int32.TryParse(values[1], out distance))
+        {
+            Debug.Log("distance: " + distance);
+        }
+
+        if (sounddelay > 0)
+        {
+            stream.Write("c");
+            sounddelay--;
+        }
+
+
+
+    }
+
+    void OpenStream()
+    {
+        if(stream != null && !stream.IsOpen)
+        {
+            stream.Open(); //Open the Serial Stream.
+        }
+       
+    }
+
+    void OnApplicationQuit()
+    {
+        if (stream != null && stream.IsOpen)
+        {
+            stream.Close(); //Close the Serial Stream.
+        }
+    }
+
+    void PlaySound()
+    {
+        sounddelay = soundlength;
+    }
+
+}
